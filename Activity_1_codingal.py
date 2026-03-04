b486b4617d18f4dea4706c443906512a3e373713
AvnishMakhija-netizen <sonampanjabi@gmail.com>
2026-03-04 19:45:52 +0530

    Add files via upload

---
+++ Activity_1_codingal.py
@@ -0,0 +1,14 @@
+tree1=98
+tree2=94
+tree3=96
+tree4=90
+tree5=95
+
+sum=tree1+tree2+tree3+tree4+tree5
+
+print("the sum of the height of the trees is ",sum)
+
+average=sum/5
+
+print("the average height of the trees is ",average)
+
