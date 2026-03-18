8ee61062d71a1c1309db995efd06172a7eebee5e
AvnishMakhija-netizen <sonampanjabi@gmail.com>
2026-03-18 19:40:30 +0530

    Add files via upload

---
+++ Divisible_codingal.py
@@ -0,0 +1,13 @@
+print("enter the numerator")
+
+num1=int(input())
+
+print("enter the dinominator")
+
+num2=int(input())
+
+if num1%num2 == 0:
+    print("the number is divisible =" +str(num1))
+
+else:
+     print("the numberis not divisible by =" +str(num2))
---
+++ Python_preference_codingal.py
@@ -0,0 +1,7 @@
+a=12
+b=15
+c=125
+d=5555
+
+ans=a*b/c-d+a
+print(ans)
