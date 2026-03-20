e6be66e1f9e88f6b631338c392774b498b71d4db
AvnishMakhija-netizen <sonampanjabi@gmail.com>
2026-03-20 20:19:36 +0530

    Add files via upload

---
+++ age_identifier.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age : "))
+
+if age >= 10:
+    if age <= 20:
+        print("your age is between 10 to 20")
+    else:
+        print("your age is not between 10 to 20")
+else:print("stop")
