27dc26ac930d80261afb785987574c986888dde2
AvnishMakhija-netizen <sonampanjabi@gmail.com>
2026-04-05 21:11:09 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,4 @@
+def greet():
+    print("hello")
+    print("how are you?")
+greet()
---
+++ Weather.py
@@ -0,0 +1,7 @@
+def greet():
+    print("the weather is pleasent in ",spring)
+    print("the weather is not same in ",autumn)
+
+spring="autumn"
+autumn="spring"
+greet()
